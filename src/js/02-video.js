2b6ec4108f7d71cb42bc89535557ba5515cd3be0
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-18 15:43:50 +0200

    timer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector("iframe");
+
+const options = {
+    id: 59777392,
+    width: 640,
+    loop: true
+}
+
+const player = new Player(iframe, options);
+
+player.setVolume(0);
+
+player.on('timeupdate', function(time) {
+    console.log(time.seconds);
+});
