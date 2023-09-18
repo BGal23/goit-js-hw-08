651cd843a9b6f132111cde96242a0a0f24ae53a9
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-18 12:48:29 +0200

    zad1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const fotoList = document.querySelector(".gallery");
+
+const addFotoList = galleryItems.map(foto => `
+    <li class="gallery__item">
+        <a class="gallery__link">
+            <img class="gallery__image"
+            src=${foto.preview} 
+            data-source=${foto.original} 
+            alt=${foto.description}/>
+        </a>
+    </li>
+`).join(" ") //----Nie dodawałem <div> </div> bo coś nieładnie to wyglądało----
+
+fotoList.insertAdjacentHTML("afterbegin", addFotoList) 
+
+fotoList.addEventListener("click", selectFoto)
+
+function selectFoto(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const modalFoto = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`);
+    modalFoto.show()
+
+    const closePhoto = (event) => {
+        if (event.key === "Escape") {
+            modalFoto.close();
+            document.removeEventListener("keydown", closePhoto)
+        };
+    };
+
+    document.addEventListener("keydown", closePhoto)
+
+};
+
