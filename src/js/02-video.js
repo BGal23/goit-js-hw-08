8922d343417b7dcf23d923605218ffd86337a9de
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-18 16:33:16 +0200

    SECOND

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector("iframe");
 
-const options = {
-    id: 59777392,
-    width: 640,
-    loop: true
-}
+const player = new Player(iframe);
 
-const player = new Player(iframe, options);
+const videoTime = throttle(value => {
+    localStorage.setItem("timer", value.seconds)
+},1000)
 
-player.on('timeupdate', function(time) {
-    localStorage.setItem("timer", time.seconds)
-});
+player.on('timeupdate', videoTime)
 
 player.setCurrentTime(localStorage.timer)
+
