fb3d9ad3f34c6a0fe42d2797b4cfac8c1daa640d
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-18 14:38:08 +0200

    second

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,19 +1,16 @@
-// Add imports above this line
-import { galleryItems } from "./gallery-items";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from "./gallery-items";
 // Change code below this line
-
-console.log(galleryItems);
 
 const fotoList = document.querySelector(".gallery");
 
 const addFotoList = galleryItems.map(foto => `
     <li class="gallery__item">
-        <a class="gallery__link">
-            <img class="gallery__image"
+        <a class="gallery__link" href=${foto.original}>
+            <img class="gallery__image" 
             src=${foto.preview} 
-            data-source=${foto.original} 
             alt=${foto.description}/>
         </a>
     </li>
@@ -21,24 +18,7 @@ const addFotoList = galleryItems.map(foto => `
 
 fotoList.insertAdjacentHTML("afterbegin", addFotoList) 
 
-fotoList.addEventListener("click", selectFoto)
-
-function selectFoto(event) {
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-
-    const modalFoto = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`);
-    modalFoto.show()
-
-    const closePhoto = (event) => {
-        if (event.key === "Escape") {
-            modalFoto.close();
-            document.removeEventListener("keydown", closePhoto)
-        };
-    };
-
-    document.addEventListener("keydown", closePhoto)
-
-};
-
+const modalFoto = new SimpleLightbox(".gallery__link", {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
