385462f44cb6b958ce73f5b737cc877707217a2c
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-18 23:15:11 +0200

    3 finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector("form");
+const mail = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const submit = document.querySelector("button")
+
+const textBox = throttle(event => {
+    if (event.target.nodeName === "INPUT") {
+        localStorage.setItem("feedbackMail", event.target.value)
+    }
+    else {
+        localStorage.setItem("feedbackTextarea", event.target.value)
+    }
+},500)
+
+if (localStorage.feedbackMail === undefined) {
+    mail.value = "";
+}
+else {
+    mail.value = localStorage.feedbackMail;
+}
+
+if (localStorage.feedbackTextarea === undefined) {
+    textarea.value = "";
+}
+else {
+    textarea.value = localStorage.feedbackTextarea;
+}
+
+const clearBox = () => {
+    localStorage.clear();
+    mail.value = "";
+    textarea.value = "";
+}
+
+form.addEventListener("input", textBox)
+submit.addEventListener("click", clearBox)
